76d2b707d0804788be04ca729eaffa65f33cca78
Keshav Kabra <ksvkabra@gmail.com>
2019-05-03 21:06:01 +0530

    saving changes to server

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -21,6 +21,7 @@ export class DishdetailComponent implements OnInit {
   next: string;
   comment: Comment;
   commentForm: FormGroup;
+  dishcopy: Dish;
 
   formErrors = {
     'author' : '',
@@ -54,7 +55,7 @@ export class DishdetailComponent implements OnInit {
 
     this.route.params.
     pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-    .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id); },
+    .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
       errMess => this.errMess = <any>errMess);
   }
 
@@ -102,6 +103,12 @@ export class DishdetailComponent implements OnInit {
     this.comment = this.commentForm.value;
     this.comment.date = new Date().toISOString();
     this.dish.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+        errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; }
+      ) 
     console.log(this.comment);
     this.comment = null;
     this.commentForm.reset({
